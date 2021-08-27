c4fb5cea5e276aef2ad31b249b8f0c5cf860d0a2
matthewjholmes <ilookup@gmail.com>
2021-08-27 16:02:43 -0600

    User Story 2 experimentation

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/artists', to: 'artists#index'
   get '/actors', to: 'actors#index'
   get '/movies', to: 'movies#index'
+  get '/artists/:id', to: 'artists#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/artists/index_spec.rb
+++ spec/features/artists/index_spec.rb
@@ -2,6 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'artists index page' do
   it 'lists all artists' do
-    
+
+        artist1 = create(:artist)
+        artist2 = create(:artist)
+        artist3 = create(:artist)
+
+        visit '/artists'
+
+        expect(current_path).to eq('/artists')
+
+        expect(page).to have_content(artist1.name)
+        expect(page).to have_content(artist2.name)
+        expect(page).to have_content(artist3.name)
+  end
+
+  it 'it finds artist by id and returns attributes' do
+    artist1 = create(:artist)
+
+    visit "/artists/#{artist1.id}"
+    save_and_open_page
+    expect(page).to have_content(artist1.name)
+    expect(page).to have_content(artist1.debut_year)
+    expect(page).to have_content(artist1.grammy_winner)
+
   end
 end
