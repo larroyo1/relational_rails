21ab1e505708b37e81cca21ea8c921e5a0634bf0
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-08-27 21:21:57 -0600

    Added ability to create new actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -7,4 +7,23 @@ class ActorsController < ApplicationController
   def show
     @actor = Actor.find(params[:id])
   end
+
+  def new
+    @new_actor = Actor.new
+  end
+
+  def create
+    actor = Actor.new(actor_params)
+
+    if actor.save
+      redirect_to actors_path
+    else
+      redirect_to actors_new_path
+    end
+  end
+
+  private
+  def actor_params
+    params.require(:actor).permit(:name, :year_first_film, :academy_award)
+  end
 end
