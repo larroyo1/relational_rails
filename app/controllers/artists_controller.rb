6eba5ab714ccd878d6287ed50d4400c6d0abd119
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-08-27 21:05:08 -0600

    Added ability to create new artist from webpage

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,4 +7,23 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
   end
+
+  def new
+    @new_artist = Artist.new
+  end
+
+  def create
+    artist = Artist.new(artist_params)
+
+    if artist.save 
+      redirect_to artists_path
+      else
+      redirect_to artists_new_path
+    end
+  end
+
+  private
+  def artist_params
+    params.require(:artist).permit(:name, :debut_year, :grammy_winner)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   #get 'artists_songs/index', to: 'artists_songs#index'
   root 'home#index'
 
+  get '/artists/new', to: 'artists#new'
+  post '/artists', to: 'artists#create'
+
   get '/artists', to: 'artists#index'
   get '/actors', to: 'actors#index'
   get '/movies', to: 'movies#index'
@@ -10,5 +13,8 @@ Rails.application.routes.draw do
   get '/songs', to: 'songs#index'
   get '/movies/:id', to: 'movies#show'
   get '/songs/:id', to: 'songs#show'
+
+
+  #get '/artists/new', to: 'artists#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
